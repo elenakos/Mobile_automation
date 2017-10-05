3e3fff65865c51e07c7a4248d6331c968d9ac788
elenakos <elenakos@users.noreply.github.com>
2017-10-04 17:00:03 -0700

    Create CalculatorTestSetup.py

---
+++ Appium/Android/Calculator/PythonTests/CalculatorTestSetup.py
@@ -0,0 +1,13 @@
+'''
+Test configuration
+'''
+class CalculatorTestSetup:
+    PLATFORM_NAME = 'Android'
+    PLATFORM_VERSION = '7.0'
+    DEVICE_NAME = 'Moto_G__5__Plus'
+    APP_PACKAGE = 'com.google.android.calculator'
+    APP_ACTIVITY= 'com.android.calculator2.Calculator'
+    HOST = 'http://localhost:4723/wd/hub'
+    
+
+        
